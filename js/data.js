ff3a86b71daf674eeb31209c66325ea69ce1db12
atsushi-maru <ikeda.atsushi00@gmail.com>
2022-10-21 06:57:58 +0900

    created data.js

---
+++ js/data.js
@@ -0,0 +1,11 @@
+// 四則演算
+console.log(45 + 18); // 63
+console.log(30 - 12); // 18
+console.log(15 * 6); // 90
+console.log(30 / 3); // 10
+console.log(45 % 8); // 5
+
+// 文字列の連結
+console.log('45' + '18'); // 4518
+console.log('今日の天気は' + '晴れ'); // 今日の天気は晴れ
+console.log('今日の天気は' + '曇り'); // 今日の天気は曇り

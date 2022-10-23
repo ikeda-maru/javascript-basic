718044cf0463b73f5c3bd15331fdbaf4d94c48b0
atsushi-maru <ikeda.atsushi00@gmail.com>
2022-10-23 16:35:54 +0900

    updated event.html & event.js

--- js/event.js
+++ js/event.js
@@ -23,12 +23,42 @@ addBtn.addEventListener('click', () => {
   parentList.appendChild(childList);
 })
 
+// count-btnというidを持つHTML要素を取得し、定数に代入する
+const countBtn = document.getElementById('count-btn');
 
-// // 新しくli要素を作成し、定数に代入する
-// const li = document.createElement('li');
+// HTML要素がクリックされたときにイベント処理を実行する
+countBtn.addEventListener('click', () => {
+  // テキストボックスに入力された文字列を取得する
+  const text = document.forms.textForm.textBox.value;
 
-// // 作成したli要素にテキストを追加する
-// li.textContent = 'これはリスト要素です';
+  // 取得した文字列の文字数を出力する
+  console.log(text.length + '文字');
+});
 
-// // ul要素の末尾にli要素を追加する
-// document.querySelector(parentList).appendChild(li);
+// area-btnというidを持つHTML要素を取得し、定数に代入する
+const areaBtn = document.getElementById('area-btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+areaBtn.addEventListener('click', () => {
+  // 選択されたラジオボタンの値を取得する
+  const area = document.forms.areaForm.area.value;
+
+  // 取得した値を出力する
+  console.log(area);
+})
+
+// os-btnというidを持つHTML要素を取得し、定数に代入する
+const osBtn = document.getElementById('os-btn');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+osBtn.addEventListener('click', () => {
+  // すべてのチェックボックスを配列風のデータで取得する
+  const items = document.forms.osForm.os;
+
+  // 繰り返し処理でチェックボックスを1つずつ取り出し、もし選択されていれば値を出力する
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].checked) {
+      console.log(items[i].value);
+    }
+  }
+});
